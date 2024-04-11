61cfed9a93347e0a7ca16b5fe665d81436ed4483
matardy <gutemberg.mendoza@epn.edu.ec>
2024-04-11 14:56:15 -0500

    feat(TransactionComponent): Add compositions API to global context

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,24 @@
   <div class="container">
     <Balance />
     <IncomeExpenses />
-    <TransactionList />
+    <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
 </template>
 
-<script>
+<script setup>
 import Balance from './components/Balance.vue'
 import Header from './components/Header.vue'; 
 import IncomeExpenses from './components/IncomeExpenses.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
-export default {
-  components: {
-    Header, 
-    Balance,
-    IncomeExpenses, 
-    TransactionList,
-    AddTransaction
-  },
-}; 
+import { ref } from 'vue'; // we need to add this to any object we want to be reactive
 
+const transactions = ref([
+        { id: 1, text: 'Flower', amount: -19.99 },
+        { id: 2, text: 'Salary', amount: 299.97 },
+        { id: 3, text: 'Book', amount: -10 },
+        { id: 4, text: 'Camera', amount: 150},
+    ]);
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,56 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li class="minus">
-            Cash <span>-$400</span><button class="delete-btn">x</button>
+        <li v-for="transaction in transactions" 
+        :key="transaction.id" 
+        :class="transaction.amount <0 ? 'minus' : 'plus' "
+        >
+            {{ transaction.text }} <span>-${{ transaction.amount }}</span><button class="delete-btn">x</button>
         </li>
-        <li class="plus">
-            Paycheck <span>$800</span><button class="delete-btn">x</button>
-        </li>
+        
     </ul>
 </template>
+
+<!-- <script>
+    // options api 
+    // export default {
+    //     data(){
+    //         return {
+    //             transactions: [
+    //                 { id: 1, text: 'Flower', amount: -19.99 },
+    //                 { id: 2, text: 'Salary', amount: 299.97 },
+    //                 { id: 3, text: 'Book', amount: -10 },
+    //                 { id: 4, text: 'Camera', amount: 150},
+    //             ],
+    //         }
+    //     }
+    // }
+    
+    // compositions api
+    export default {
+        setup(){
+            const transactions = [
+                    { id: 1, text: 'Flower', amount: -19.99 },
+                    { id: 2, text: 'Salary', amount: 299.97 },
+                    { id: 3, text: 'Book', amount: -10 },
+                    { id: 4, text: 'Camera', amount: 150},
+                ]; 
+                return {
+                    transactions,
+                }
+        }
+    }
+</script> -->
+<script setup>
+    // this define the prop that was pass out in the App.vue
+
+    import { defineProps } from 'vue'; 
+
+    const props = defineProps({
+        transactions: {
+            type: Array, 
+            required: true
+        }
+    })
+
+</script>
